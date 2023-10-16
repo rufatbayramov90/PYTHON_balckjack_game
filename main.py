3d939f5d89bdea92c3d5c6a05f40d6cc55a44e2a
rufatbayramov90 <rufatbyrmv@gmail.com>
2023-10-16 12:38:31 +0400

    random card selection

---
+++ main.py
@@ -0,0 +1,6 @@
+import random
+
+def deal_card():               # siyahidan rastgele kart secib return edecem
+    cards = [ 11,2,3,4,5,6,7,8,9,10,10,10,10]
+    card = random.choice(cards)    
+    return card 
